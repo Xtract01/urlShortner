53ce9ee38c72d95cd83edb43b57d891952b4ee85
Xtract01 <meena.balwir149@gmail.com>
2025-09-03 01:06:25 +0530

    LoginForm

--- FRONTEND/src/pages/HomePage.jsx
+++ FRONTEND/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import UrlForm from "../components/urlForm";
+import UrlForm from "../components/urlForm.jsx";
+
 const HomePage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
